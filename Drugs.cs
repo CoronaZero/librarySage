6c6e8504de780c0ca436b9a077b8fee0eeb30a13
Corona Zero <corona@dreamweaver.cc>
2024-06-25 09:31:44 +0800

    添加项目文件。

---
+++ Drugs.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace drugSystemBlazor;
+public class DSDBcontext : DbContext
+{
+    public DbSet<Drug> Drugs { get; set; }
+    public DbSet<Prescription> Prescriptions { get; set; }
+    public string DBpath { get; set; }
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlite($"Data Source={this.DBpath}"); //设置使用SQLite
+
+    }
+
+    public DSDBcontext()
+    {
+        string path = Path.Join(Environment.CurrentDirectory, "Drug.db");
+        this.DBpath = path; //设置数据库路径
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Drug>().HasIndex(m => m.Name).HasDatabaseName("DrugName"); //为Drug表Name属性添加索引
+        modelBuilder.Entity<Prescription>().HasIndex(p => p.PatientName).HasDatabaseName("PatientNameIndex"); //Prescription表PatientName添加索引
+    }
+
+}
+public class Drug 
+{
+    [Key]
+    public int ID { get; set; } // 药品ID
+    [Required]
+    public string Name { get; set; } //药品名称
+    [Required]
+    public float Price { get; set; } //药品单价
+    [Required]
+    public int Quantity { get; set; } //药品库存数量
+}
+
+public class Prescription
+{
+    [Key]
+    public int ID { get; set; } //处方单ID
+    [Required]
+    public string PatientName { get; set; } //处方病人姓名
+    [Required]
+    public List<PrescriptionDrugs> Drugs { get; set; } //处方药品详单
+
+    [Required]
+    public bool IsPatientTakes { get; set; } //病人是否拿药
+
+}
+
+public class PrescriptionDrugs
+{
+    [Key]
+    public int ID { get; set; } //无意义,用于作为主键
+    [Required]
+    public int DrugID { get; set; } //药品ID
+    [Required]
+    [ForeignKey("DrugID")]
+    public Drug Drug { get; set; } //药品详情
+    [Required]
+    public int Quantity { get; set; } //需要拿取的这个药品的数量
+
+}
---
+++ Migrations/20240624130506_InitialCreate.cs
@@ -0,0 +1,102 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace drugSystemBlazor.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Drugs",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(type: "TEXT", nullable: false),
+                    Price = table.Column<float>(type: "REAL", nullable: false),
+                    Quantity = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Drugs", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Prescriptions",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    PatientName = table.Column<string>(type: "TEXT", nullable: false),
+                    IsPatientTakes = table.Column<bool>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Prescriptions", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "PrescriptionDrugs",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    DrugID = table.Column<int>(type: "INTEGER", nullable: false),
+                    Quantity = table.Column<int>(type: "INTEGER", nullable: false),
+                    PrescriptionID = table.Column<int>(type: "INTEGER", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PrescriptionDrugs", x => x.ID);
+                    table.ForeignKey(
+                        name: "FK_PrescriptionDrugs_Drugs_DrugID",
+                        column: x => x.DrugID,
+                        principalTable: "Drugs",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_PrescriptionDrugs_Prescriptions_PrescriptionID",
+                        column: x => x.PrescriptionID,
+                        principalTable: "Prescriptions",
+                        principalColumn: "ID");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "DrugName",
+                table: "Drugs",
+                column: "Name");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PrescriptionDrugs_DrugID",
+                table: "PrescriptionDrugs",
+                column: "DrugID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PrescriptionDrugs_PrescriptionID",
+                table: "PrescriptionDrugs",
+                column: "PrescriptionID");
+
+            migrationBuilder.CreateIndex(
+                name: "PatientNameIndex",
+                table: "Prescriptions",
+                column: "PatientName");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "PrescriptionDrugs");
+
+            migrationBuilder.DropTable(
+                name: "Drugs");
+
+            migrationBuilder.DropTable(
+                name: "Prescriptions");
+        }
+    }
+}
